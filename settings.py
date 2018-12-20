1f409bcd1b4a0470742c2653532786a9650c0e16
TMTS-Projects <sudhanshu.j@talentmagnifia.com>
2018-12-20 17:25:01 +0530

    Base Model Done by Sudhanshu

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+pymysql://sa:TMTSserver@31@34.205.144.198:9000/travel_to_trip', echo=True)
+engine = create_engine('mysql+pymysql://sa:TMTSserver@31@3.84.25.27:9000/travel_to_trip', echo=True)
 Base = declarative_base()
 
 def sessionRepo():
