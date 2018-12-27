93b41dff65905a5589722d7bf778a6a777a986f8
TMTS-Projects <venu.s@talentmagnifia.com>
2018-12-27 15:42:57 +0530

    Done by venu 27/12/2018_2

--- static/js/project/HomePage.js
+++ static/js/project/HomePage.js
@@ -18,9 +18,18 @@ function get_search_result(){
 var input_id = localStorage.getItem("typeId");
 var id = "text_search_" + input_id;
 var search_json = JSON.stringify( { "input" : document.getElementById(id).value});
+var text_id = "text_search_" + input_id;
+var checkin_id = "checkin_" + input_id;
+var checkout_id = "checkout_" + input_id;
+var rooms_id = "rooms_" + input_id;
+var search_json = JSON.stringify( { "input" : document.getElementById(text_id).value,
+                                   "checkin" : document.getElementById(checkin_id).value,
+                                   "checkout" : document.getElementById(checkout_id).value,
+                                   "rooms" : document.getElementById(rooms_id).value
+                                   }
+                                );
 send_json('POST',"/SearchedMenus",search_json);
 }
-
 
 
 function send_json(send_type,send_url,send_data){
